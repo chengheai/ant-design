f0926c329424cafaff64e0211a0f085788a0183d
chengheai <842385139@qq.com>
2018-08-24 14:58:17 +0800

    Signed-off-by: chengheai <842385139@qq.com>

--- src/routes/Avatar.jsx
+++ src/routes/Avatar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Avatar, Badge } from 'antd';
 
 
 class AvatarC extends React.Component {
@@ -8,8 +9,16 @@ class AvatarC extends React.Component {
 
     return (
       
-      <div>
-        Avatar
+      <div className='my-flex-item'>
+        <Avatar icon="user" />
+        <Avatar>U</Avatar>
+        <Avatar>USER</Avatar>
+        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+        <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
+        <Avatar style={{ backgroundColor: '#87d068' }} icon="user" />
+        <span style={{ marginRight: 24 }}>
+          <Badge count={999}><Avatar shape="square" icon="user" /></Badge>
+        </span>
       </div>
     );
   }
--- src/routes/TimePicker.jsx
+++ src/routes/TimePicker.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { connect } from 'dva';
+import { TimePicker } from "antd";
 
-
+import moment from 'moment'
 class TimePickerC extends React.Component {
-
+  onChange = (time, timeString) =>{
+    console.log(time, timeString)
+  }
   render(){
 
     return (
       
       <div>
-        TimePicker
+        <TimePicker onChange={this.onChange} defaultOpenValue={moment('00:00:00', 'HH:mm:ss')} />
       </div>
     );
   }
